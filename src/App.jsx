ab91fc161d80b93871f36eaaa103509c853f09ed
Shehan wijerathna <72376873+ShehanRW@users.noreply.github.com>
2026-06-07 01:30:01 +0530

    fix: BrowserRouter with 404 redirect for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layouts/NavBar'
 import Home from './pages/Home'
 import Destinations from './pages/Destinations'
